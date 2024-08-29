0ee416415acd93e075c244e7f3cb72eeb17db9af
anjeh24 <junez24@yahoo.com>
2024-08-29 10:40:00 -0400

    iitial commit after trying to make landing page look decent

---
+++ salad-bar/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { useState } from 'react'
+// import {useState, useEffect} from 'react'
+import Landingpage from './components/Landingpage'
+import vegdoddle from './mernpics/vegdoddle.jpg'
+
+  import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+     <div className='mainapp_div'>
+        
+        <Landingpage/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ salad-bar/src/components/Landingpage.jsx
@@ -0,0 +1,45 @@
+// import React from 'react'
+import React from 'react'
+// import { useEffect } from 'react';
+import {TiShoppingCart} from "react-icons/ti"
+ import {Link} from "react-router-dom";
+ import rando1 from '../mernpics/rando1.jpg';
+ import rando2 from '../mernpics/rando2.jpg';
+ import rando3 from '../mernpics/rando3.jpg'
+ 
+function Landingpage() {
+  return (
+    <div>
+        
+      <div className='nav_bar'>
+         <li>About</li>
+         <li>Menuingredients</li>
+         <li>Login</li>
+         <li>Ordersubmission</li>
+         <li className='la_cart'><TiShoppingCart/></li>
+        
+      </div> 
+
+      <div className='title'>
+        <h1 className='headcol'>Deli Greens</h1>
+        <h2 className='headcol'>"Come for the experience, stay for the flavor and wellness....."</h2>
+      </div>
+
+      <div className='heroim'>
+      <img  className='img1' src={rando2} alt="produce" style={{width: '30%', height: '15%'}} />
+        {/* <ul className='ulfooter'> */}
+            {/* <li><img src={rando1} alt="produce" style={{width: '15%', height: '15%'}}/></li>
+            <li><img src={rando2} alt="produce" style={{width: '15%', height: '15%'}} /></li>
+            <li><img src={rando3} alt="produce" style={{width: '15%', height: '15%'}}/></li> */}
+           
+        {/* </ul> */}
+      </div>
+
+       <div className='foot_div'>
+      <footer className='the_footer'><h6>Made with &#x2764;&#xfe0f; by Anjeh Mbah </h6></footer>
+      </div> 
+    </div>
+  )
+}
+
+export default Landingpage
